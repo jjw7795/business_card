dfb8694554ef6ce0fcc4a06bbc5b990f8989dd03
jjw7795 <jjw7795@naver.com>
2021-05-18 22:49:44 +0900

    Updates

---
+++ precache-manifest.a1272b748c0d493a27dc02c297362805.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44f1a8100544641fb2d662072f7e328a",
+    "url": "/business_card/index.html"
+  },
+  {
+    "revision": "c20259d6f2e2d34f606d",
+    "url": "/business_card/static/css/main.1f300ba9.chunk.css"
+  },
+  {
+    "revision": "81e08e0633a90e5086d2",
+    "url": "/business_card/static/js/2.fc2cfb09.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/business_card/static/js/2.fc2cfb09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c20259d6f2e2d34f606d",
+    "url": "/business_card/static/js/main.1dcaf822.chunk.js"
+  },
+  {
+    "revision": "ea506fe1a35dc23cee8c",
+    "url": "/business_card/static/js/runtime-main.c06000fa.js"
+  }
+]);
